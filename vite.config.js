fe27a3c7c76bcbacd4303088222d531cdb63e8ab
Babajide789 <yahayababajide@gmail.com>
2025-08-04 19:50:50 -0400

    Fix build issue for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './', // or wherever your index.html is
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
 })
